7a6a3eee9665792616bd91a608d9f04ea3bdab27
JackSerafini <94188919+JackSerafini@users.noreply.github.com>
2021-11-23 15:23:28 +0000

    Shampoo Sales

---
+++ lezione3.py
@@ -0,0 +1,20 @@
+#funzione che somma tutte le vendite
+def somma():
+    values = []
+    somma = 0
+    my_file = open('shampoo_sales.csv', 'r')
+    #dividere la data dalle vendite
+    for line in my_file:
+        elements = line.split(',')
+        if elements[0] != 'Date':
+            date = elements[0]
+            value = elements[1]
+
+            #creare una liste delle vendite
+            values.append(float(value))
+    #sommo tutti i valori
+    for value in values:
+        somma = somma + value
+    return somma
+
+print(somma())
